fd987cd4a6a3562a75cd072d0c871659bb5e58f3
Patrick Szalewicz <info@sysop.top>
2018-02-05 16:23:50 +0100

    installer added

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+    echo "Not running as root, please run this script with root rights!"
+    exit 1
+fi
+
+mkdir /opt/RasPiTimelapse
+mkdir /home/pi/RasPiTimelapse
+cd /opt/RasPiTimelapse
+wget https://raw.githubusercontent.com/szalewicz/RasPiTimelapse/master/take_photo.sh
+wget https://raw.githubusercontent.com/szalewicz/RasPiTimelapse/master/create_timelapse.sh
+chmod +x take_photo.sh 
+chmod +x create_timelapse.sh
+
+echo "Done, you can now start taking pictures manually or install a cron job"
